d94365d03939e9d45f194d34d9f3791133c99d49
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-02 23:23:14 +0100

    electricity bills endpoint

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -33,15 +33,51 @@ const buyDataSubcription = async (req, res) => {
         }
     } catch(error) {
         logger.error("Error in data subscription:", error);
-        return res.status(500).json({ error: "Internal server error." });
+        return res.status(500).json({ success: false, error: "Internal server error." });
     }
 }   
+
+const getAllDataTransactions = async (req, res) => {
+    logger.info("Received request for all data transactions") 
+    try {
+        const response = await axios.get(`${process.env.EXTERNAL_BACKEND_DOMAIN}/data`, {
+            headers: { 
+                Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`, 
+            }
+        })
+        return res.status(200).json(response.data)
+    } catch(error){
+        logger.error("Error fetching all data transactions:", error);
+        return res.status(500).json({ success: false, error: "Internal server error." })
+    }
+}
+
+const queryDataTransaction = async (req, res) => {
+    logger.info("Received request for data transaction query")
+    const { transactionId } = req.params;
+    if (!transactionId) {
+        logger.error("Transaction ID is required for querying data transaction")
+        return res.status(400).json({ error: "Transaction ID is required." })
+    }     
+
+    try {
+        const response = await axios.get(`${process.env.EXTERNAL_BACKEND_DOMAIN}/data/${transactionId}`, {
+            headers: { 
+                Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`,
+            }
+        })
+        return res.status(200).json(response.data)
+    } catch(error) {
+        logger.error("Error querying data transaction:", error)
+        return res.status(500).json({ success: false, error: "Internal server error." })
+    }
+}
 
 const buyAirtimeSubscription = async (req, res) => {
     logger.info("Received request for airtime subscription")
     const { network, phone, amount, airtime_type } = req.body 
     if (!network || !phone || !amount || !airtime_type) {
-        return res.status(400).json({ error: "Network, phone, airtime type and amount are required."});
+        return res.status(400).json({ error: "Network, phone, airtime type and amount are required."})
     }
 
     try {
@@ -67,8 +103,81 @@ const buyAirtimeSubscription = async (req, res) => {
         }
     } catch(error) {
         logger.error("Error in airtime subscription:", error);
-        return res.status(500).json({ error: "Internal server error." });
+        return res.status(500).json({ success: false, error: "Internal server error." })
     }
 }
 
-export { buyDataSubcription, buyAirtimeSubscription }
+const queryAirtimeTransaction = async (req, res) => {
+    logger.info("Received request for airtime transaction query")
+    const { transactionId } = req.params;
+    if (!transactionId) {
+        logger.error("Transaction ID is required for querying airtime transaction")
+        return res.status(400).json({ error: "Transaction ID is required." })
+    }     
+
+    try {
+        const response = await axios.get(`${process.env.EXTERNAL_BACKEND_DOMAIN}/topup/${transactionId}`, {
+            headers: { 
+                Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`,
+            }
+        })
+        return res.status(200).json(response.data)
+    } catch(error) {
+        logger.error("Error querying airtime transaction:", error)
+        return res.status(500).json({ success: false, error: "Internal server error." })
+    }
+}
+
+const payElectricityBills = async (req, res) => {
+    const { disco_name, amount, meter_number, meter_type } = req.body
+    if (!disco_name || !amount || !meter_number || !meter_type) {
+        return res.status(400).json({ error: "Disco name, amount, meter number, and meter type are required." });
+    }
+    try {
+        const response = await axios.post(`${process.env.EXTERNAL_BACKEND_DOMAIN}/billpayment`, 
+            {
+                disco_name,
+                amount,
+                meter_number,
+                meter_type,
+            }, {
+                headers: {
+                    Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`,
+                }
+            })
+
+        if (response.status === 200) {
+            logger.info("Electricity bill payment successful:", response.data);
+            return res.status(200).json(response.data);
+        } else {
+            logger.error("Electricity bill payment failed with status:", response.status);
+            return res.status(response.status).json({ error: "Failed to pay electricity bill." });
+        }
+    } catch(error) {
+        logger.error("Error in electricity bill payment:", error);
+        return res.status(500).json({ success: false, error: "Internal server error." });
+    }
+}
+
+const queryElectricityBill = async (req, res) => {
+    logger.info("Received request for electricity transaction query")
+    const { transactionId } = req.params;
+    if (!transactionId) {
+        logger.error("Transaction ID is required for querying electricity transaction")
+        return res.status(400).json({ error: "Transaction ID is required." })
+    }     
+
+    try {
+        const response = await axios.get(`${process.env.EXTERNAL_BACKEND_DOMAIN}/billpayment/${transactionId}`, {
+            headers: { 
+                Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`,
+            }
+        })
+        return res.status(200).json(response.data)
+    } catch(error) {
+        logger.error("Error querying electricity transaction:", error)
+        return res.status(500).json({ success: false, error: "Internal server error." })
+    }
+}
+
+export { buyDataSubcription, getAllDataTransactions, queryDataTransaction, buyAirtimeSubscription, queryAirtimeTransaction, payElectricityBills, queryElectricityBill }
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,12 +1,18 @@
 import { Router } from "express"
-import { buyDataSubcription, buyAirtimeSubscription } from "../controller/account.controller.js"
+import { buyDataSubcription, buyAirtimeSubscription, getAllDataTransactions, queryDataTransaction, queryAirtimeTransaction, payElectricityBills, queryElectricityBill} from "../controller/account.controller.js"
 import { authenticateUser } from "../middleware/auth.middleware.js"
 import validateDataReqBody from "../middleware/data.schema.js"
 import validateAirtimeReqBody from "../middleware/airtime.schema.js"
+import validateElectricityReqBody from "../middleware/electricity.schema.js"
 
 const router = Router()
 
 router.post("/data", authenticateUser, validateDataReqBody, buyDataSubcription)
 router.post("/airtime", authenticateUser, validateAirtimeReqBody, buyAirtimeSubscription)
+router.get("/data-history", authenticateUser, getAllDataTransactions)
+router.get("/query-data/:transactionId", authenticateUser, queryDataTransaction)
+router.get("/query-airtime/:transactionId", authenticateUser, queryAirtimeTransaction)
+router.post("/electricity", authenticateUser, validateElectricityReqBody, payElectricityBills)
+router.get("/query-electricity-bill", authenticateUser, queryElectricityBill)
 
 export default router
