57965bfef60d017c7d879116ebdc486be56d2715
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-02 22:32:55 +0100

    airtime subscription controller

--- src/controller/account.controller.js
+++ src/controller/account.controller.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import dotenv from "dotenv" 
 dotenv.config()
 
+
 const buyDataSubcription = async (req, res) => {
     logger.info("Received request for data subscription")
     const { network, phone, plan } = req.body 
@@ -35,5 +36,39 @@ const buyDataSubcription = async (req, res) => {
         return res.status(500).json({ error: "Internal server error." });
     }
 }   
+
+const buyAirtimeSubscription = async (req, res) => {
+    logger.info("Received request for airtime subscription")
+    const { network, phone, amount, airtime_type } = req.body 
+    if (!network || !phone || !amount || !airtime_type) {
+        return res.status(400).json({ error: "Network, phone, airtime type and amount are required."});
+    }
+
+    try {
+        const response = await axios.post(`${process.env.EXTERNAL_BACKEND_DOMAIN}/topup`, 
+            {
+            network,
+            phone,
+            amount,
+            airtime_type,
+            ported_number: true
+        },   {
+             headers: {
+                 Authorization: `Token ${process.env.EXTERNAL_BACKEND_API_KEY}`,
+                }
+            })
+
+        if (response.status === 200) {
+            logger.info("Airtime subscription successful:", response.data);
+            return res.status(200).json(response.data);
+        } else {
+            logger.error("Airtime subscription failed with status:", response.status);
+            return res.status(response.status).json({ error: "Failed to subscribe to airtime." });
+        }
+    } catch(error) {
+        logger.error("Error in airtime subscription:", error);
+        return res.status(500).json({ error: "Internal server error." });
+    }
+}
 
 export { buyDataSubcription }
--- src/routes/account.route.js
+++ src/routes/account.route.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { buyDataSubcription } from "../controller/account.controller.js"
+import { buyDataSubcription, buyAirtimeSubscription } from "../controller/account.controller.js"
 import { authenticateUser } from "../middleware/auth.middleware.js"
 import validateDataReqBody from "../middleware/data.schema.js"
+import validateAirtimeReqBody from "../middleware/airtime.schema.js"
 
 const router = Router()
 
 router.post("/data", authenticateUser, validateDataReqBody, buyDataSubcription)
+router.post("/airtime", authenticateUser, validateAirtimeReqBody, buyAirtimeSubscription)
 
 export default router
