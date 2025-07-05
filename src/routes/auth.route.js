7992102aaba3406e79335ada0832cf43640c2d7e
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-07-05 21:17:40 +0100

    logout

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -2,11 +2,11 @@ import { Router } from "express"
 import { loginUser, logoutUser, registerUser } from "../controller/auth.controller.js"
 import validateSignUpInput from "../middleware/signup.schema.js"
 import validateLoginInput from "../middleware/login.schema.js"
-
+import { authenticateUser } from "../middleware/auth.middleware.js"
 const router = Router()
 
 router.post("/register", validateSignUpInput, registerUser)
 router.post("/login", validateLoginInput, loginUser)
-router.post("/logout", logoutUser)
+router.post("/logout", authenticateUser, logoutUser)
 
 export default router
